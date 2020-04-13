1f1fbe7e40257eb5f0455a95928d832b910ae43d
Administrator <Administrator@I9KQV0J4L9ESBFT>
2020-04-13 11:13:28 +0800

    test

--- src/helloaworld/HelloaWorld.java
+++ src/helloaworld/HelloaWorld.java
@@ -4,7 +4,8 @@ public class HelloaWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helloaworld");
+		System.out.println("helloaworld,edit for test");
+		
 	}
 
 }
